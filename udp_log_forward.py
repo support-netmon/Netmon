839351485a7510ea34e33b48965b99c3f4a20518
support-netmon <jaspreet.singh@net-mon.net>
2016-04-20 17:26:34 +0530

    Update udp_log_forward.py

--- udp_log_forward.py
+++ udp_log_forward.py
@@ -1,12 +1,11 @@
 from scapy.all import *
 import random
 import sys
-from scapy.base_classes import Gen, Net, SetGen
+from scapy.base_classes import Gen, Net, SetGen #Comment out this line if the you get a error about Setgen
 
 def send_p(src_ip,data):
     Source_IP= src_ip
-    #Dest_ip = '192.168.11.52'
-    Dest_ip = '192.168.1.30'
+    Dest_ip = 'IP'
     Src_Port = random.randint(5000,7000)
     pyload = data
     spoofed_packet = fragment(IP(src=Source_IP, dst=Dest_ip) / UDP(sport=Src_Port, dport=514) / Raw(pyload),65535)
